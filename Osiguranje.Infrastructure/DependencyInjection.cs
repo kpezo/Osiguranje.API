5f7060d415f53cebc5c834a4a42b8d86e02ac39b
kpezo <kresimir.pezo@gmail.com>
2022-11-08 14:20:22 +0100

    Added -user-secrets -IDateTimeProvider(for UnitTesting) -JwtGenerator -

--- Osiguranje.Infrastructure/DependencyInjection.cs
+++ Osiguranje.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Osiguranje.Application.Common.Interfaces.Authentication;
+using Osiguranje.Application.Common.Interfaces.Services;
+using Osiguranje.Infrastructure.Authentication;
+using Osiguranje.Infrastructure.Services;
 
 namespace Osiguranje.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, Microsoft.Extensions.Configuration.ConfigurationManager configuration)
         {
+            services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+
+            services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+            services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+
             return services;
         }
     }
---
+++ Osiguranje.Infrastructure/Services/DateTimeProvider.cs
@@ -0,0 +1,14 @@
+﻿using Osiguranje.Application.Common.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osiguranje.Infrastructure.Services
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+}
